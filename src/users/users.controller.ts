dc9c8bfe3fd5516ba0710202d7cf0732e3c380d6
omykkru089 <sasha20031218@gmail.com>
2025-05-30 13:25:18 +0200

    v0.056 arreglos generales

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,27 +8,27 @@ import { Role } from 'src/common/enums/rol.enum';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
+  @Auth(Role.ADMIN)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
-
+  @Auth(Role.ADMIN)
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
-
+  @Auth(Role.USER)
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.usersService.findOne(id);
   }
-
+  @Auth(Role.USER)
   @Patch(':id')
   update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
-
+  @Auth(Role.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.usersService.remove(+id);
